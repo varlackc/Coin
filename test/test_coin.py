a73b507506e6ec2205113ff1b5dbc7e9e5454366
varlackc <varlackc@mail.gvsu.edu>
2025-09-07 21:59:11 -0500

    add the tests to get face and set face

--- test/test_coin.py
+++ test/test_coin.py
@@ -4,3 +4,12 @@ from coin import Coin
 class TestCoin(unittest.TestCase):
     def test_coin_flip(self):
         self.assertIsNotNone(Coin().flip())
+    
+    def test_coin_get_face(self):
+        self.assertIsNotNone(Coin().get_face())
+    
+    def test_coin_set_face(self):
+        coin = Coin()
+        coin.set_face("Head")
+        self.assertEqual(coin.get_face(), "Head")
+        
