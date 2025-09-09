75e533a42b17126b367837eb6bd5a228860e6847
varlackc <varlackc@mail.gvsu.edu>
2025-09-09 13:26:43 -0500

    expand test cases for the test coin set class

--- test/test_coin_set.py
+++ test/test_coin_set.py
@@ -1,10 +1,23 @@
 import unittest
-from coin import CoinSet
+from coin import CoinSet, Coin
 
 
 class TestCoinSet(unittest.TestCase):
+    def test_CoinSet_add_coin(self):
+        c1 = Coin()
+        cs = CoinSet()
+        cs.add_coin(c1)
+        self.assertGreater(len(cs.coin_collection), 0)
+        
+    def test_CoinSet_add_coins(self):
+        c1 = Coin()
+        c2 = Coin()
+        cs = CoinSet()
+        cs.add_coins([c1, c2])
+        self.assertGreater(len(cs.coin_collection), 1)
+
     def test_CoinSet_flip(self):
-        self.assertIsNotNone(CoinSet().flip())
+        self.assertIsNotNone(CoinSet().coin_collection)
         
     def test_coinset_get_face(self):
         self.assertIsNotNone(CoinSet().get_faces())
