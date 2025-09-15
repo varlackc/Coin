f078bc18236794160a428664f065812825072bb2
varlackc <varlackc@mail.gvsu.edu>
2025-09-14 23:29:05 -0500

    provide comment explaining the get_face method

--- coin/coin.py
+++ coin/coin.py
@@ -14,6 +14,7 @@ class Coin:
         return str(self.face)
     
     def get_face(self):
+        # This method is used to get the face value on the coin
         return str(self.face)
     
     def set_face(self, value: str):
