7e05a4f19e1aef8d1cfabc9b815dbf4f6e26c9b7
varlackc <varlackc@mail.gvsu.edu>
2025-09-20 23:36:26 -0500

    add a comment to the test_coin_set_head method

--- test/test_coin.py
+++ test/test_coin.py
@@ -31,6 +31,8 @@ class TestCoin(unittest.TestCase):
         self.assertIsNotNone(Coin().get_head())
  
     def test_coin_set_head(self):
+        # test to verify that the head value
+        # can be modified
         coin = Coin()
         coin.set_head("H")
         self.assertIsNotNone(coin.get_head())
