3111450d255f2e1bc0ce26c47de7eaa06bab3857
varlackc <varlackc@mail.gvsu.edu>
2025-10-29 19:26:31 -0500

    consolidate comments in the test_coin_get_tails method

--- test/test_coin.py
+++ test/test_coin.py
@@ -55,8 +55,10 @@ class TestCoin(unittest.TestCase):
         self.assertIsNotNone(coin.get_head())
         
     def test_coin_get_tails(self):
-        # test to verify that the tails value
-        # can be modified
+        """
+        test to verify that the tails value
+        can be modified
+        """
         self.assertIsNotNone(Coin().get_tails())
     
     def test_coin_set_tails(self):
