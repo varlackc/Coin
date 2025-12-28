6b240276deb7ce9b11b5342d726c31cc0a6319a4
varlackc <varlackc@mail.gvsu.edu>
2025-12-27 18:44:53 -0600

    add type hints to the test_coin_set_coin method

--- test/test_coin.py
+++ test/test_coin.py
@@ -79,7 +79,7 @@ class TestCoin(unittest.TestCase):
         coin.set_coin("H", "T")
         self.assertEqual([coin.get_head(), coin.get_tails()], ["H","T"])
     
-    def test_coin_get_coin(self):
+    def test_coin_get_coin(self) -> None:
         """
         test to verify that the coin value
         can be reached
